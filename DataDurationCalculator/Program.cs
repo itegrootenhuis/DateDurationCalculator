e38d669f126ee76f3e3aae8601c8b5f707cb5dc9
Unknown <itegrootenhuis@bizstream.com>
2018-01-09 11:08:23 -0500

    finish date calculator

--- DataDurationCalculator/Program.cs
+++ DataDurationCalculator/Program.cs
@@ -16,17 +16,41 @@ namespace DataDurationCalculator
 			GetDates();
 		}
 
+		private static void QuitConsoleApp()
+		{
+			Console.WriteLine();
+			Console.WriteLine();
+			Console.WriteLine("Press R to repeat or any other key to exit the app.");
+			ConsoleKeyInfo quitKey = Console.ReadKey();
+
+			if (quitKey.Key.ToString().ToLower() == "r")
+			{
+				Console.Clear();
+				GetDates();
+			}
+		}
+
 		private static void GetDates()
 		{
 			Console.WriteLine("Please enter two dates: ");
 
 			Console.Write("Date 1: ");
-			date1 = DateTime.Parse(Console.ReadLine());
+			if (!DateTime.TryParse(Console.ReadLine(), out date1))
+				InvalidDate(); 
 
 			Console.Write("Date 2: ");
-			date2 = DateTime.Parse(Console.ReadLine());
+			if (!DateTime.TryParse(Console.ReadLine(), out date2))
+				InvalidDate();
 
 			CalculateDifferenceInDates(date1, date2);
+		}
+
+		private static void InvalidDate()
+		{
+			Console.Clear();
+			Console.WriteLine(string.Format("You entered in an invalid date. Please use an acceptable date format like 1-1-18 or 1/1/18 \n\n"));
+			GetDates();
+			
 		}
 
 		private static void CalculateDifferenceInDates(DateTime date1, DateTime date2)
@@ -52,7 +76,8 @@ namespace DataDurationCalculator
 
 		private static void BuildAnswer(DateTime date1, DateTime date2, double totalDays, double totalHours, double totalMinutes)
 		{
-			Console.WriteLine(string.Format("The time difference between {0} and {1} is {2} days - {3} hours - {4} minutes."), date1, date2, totalDays, totalHours, totalMinutes);
+			Console.WriteLine(string.Format("The time difference between {0} and {1} is {2} days - {3} hours - {4} minutes.", date1.ToString("d"), date2.ToString("d"), totalDays, totalHours, totalMinutes));
+			QuitConsoleApp();
 		}
 	}
 }
